5d2b5ba11b922d8912cb2a4da2aefbace341c71d
Hudson Green <hudsongreen27@gmail.com>
2023-06-24 17:48:45 -0500

    FPS Display

--- render.py
+++ render.py
@@ -1,9 +1,13 @@
 import pygame
 
+
 def render_stack(surf, images, pos, rotation, spreadx=0, spready=1):
     for i, img in enumerate(images):
         rotated_img = pygame.transform.rotate(img, rotation)
-        surf.blit(rotated_img, (
-        pos[0] - rotated_img.get_width() // 2 - i * spreadx,
-        pos[1] - rotated_img.get_height() // 2 - i * spready
-        ))
+        surf.blit(
+            rotated_img,
+            (
+                pos[0] - rotated_img.get_width() // 2 - i * spreadx,
+                pos[1] - rotated_img.get_height() // 2 - i * spready,
+            ),
+        )
